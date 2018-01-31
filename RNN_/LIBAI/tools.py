da739b0ecf947549b84c30c37690af885f545933
Matafight <guo_sc@foxmail.com>
2018-01-31 15:26:55 +0800

    add tools.py

--- RNN_/LIBAI/tools.py
+++ RNN_/LIBAI/tools.py
@@ -2,5 +2,13 @@
 '''
 This is a collection of many reuseable functions
 '''
-
+import seaborn as sen
+import matplotlib.pyplot as plt
 #draw curve w.r.t iterations
+def draw_curve(vec1,legendfir = 'First',xlabel='x',ylabel='y',vec2=None,legendsec='Second',save_path='.'):
+    ax = sen.tsplot(vec1,color='r',condition=legendfir,legend=True)
+    if vec2!=None:
+        ax2 = sen.tsplot(vec2,color='b',condition=legendsec,legend=True)
+    ax.set(xlabel=xlabel,ylabel=ylabel)
+    plt.savefig(save_path+'/test.png')
+     
